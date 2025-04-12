cf7cf65dc05eda2055d2afb3444ae9de50717d6d
Michael Sturm <michael.c.sturm@gmail.com>
2025-04-11 18:30:02 -0700

    Added emotions

---
+++ emotion_pickup.gd
@@ -0,0 +1,22 @@
+extends Node2D
+class_name emotion_pickup 
+
+enum emotions {FEAR, JOY, DEPRESSION, ANGER, LOVE}
+@export var emotion: emotions 
+@export var player : PlayerCharacter
+
+
+
+func _on_collision_shape_2d_child_entered_tree(node: Node) -> void:
+	if Node == player:
+		if emotion == emotions.FEAR:
+			Global.fear = true
+		if emotion == emotions.JOY:
+			Global.joy = true
+		if emotion == emotions.DEPRESSION:
+			Global.depression = true
+		if emotion == emotions.ANGER:
+			Global.anger = true
+		if emotion == emotions.LOVE:
+			Global.love = true
+		
