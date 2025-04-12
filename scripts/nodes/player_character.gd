38ce7987319d91556e5f55aaf3a34e5dc7cf7e43
Ryly Dou <rylydou@gmail.com>
2025-04-11 19:45:28 -0500

    add scene folder

---
+++ scripts/nodes/player_character.gd
@@ -0,0 +1,487 @@
+class_name PlayerCharacter extends CharacterBody2D
+
+
+signal died()
+
+
+@export var allow_midair_flip = true
+
+@export_group("Movement", "move_")
+@export var move_speed := 4.0
+
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var move_acc_ticks := 8.0
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var move_dec_ticks := 6.0
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var move_opp_ticks := 4.0
+
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var move_acc_air_ticks := 16.0
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var move_dec_air_ticks := 12.0
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var move_opp_air_ticks := 8.0
+
+@export_group("Gravity and Jump")
+@export_range(0, 16, 1, "or_greater", "suffix:px") var jump_height_min := 1.0
+@export_range(0, 16, 1, "or_greater", "suffix:px") var jump_height_max := 3.0
+@export_range(0, 120, 1, "or_greater", "suffix:ticks") var jump_ticks := 30.0
+@export_range(0, 120, 1, "or_greater", "suffix:ticks") var fall_ticks := 30.0
+var fall_gravity := 0.0
+var jump_gravity := 0.0
+var jump_velocity_min := 0.0
+var jump_velocity_max := 0.0
+@export var step_time := 0.2
+var step_timer := 0.0
+
+## The maximum falling speed based on the max jump height
+@export var max_fall_ratio := 2.0
+var max_fall_speed := 0.0
+var is_jumping := false
+
+@export var bonk_bounce := 0.0
+
+@export_group("Assists")
+## The amount of time the player can still jump after leaving a platform, see "Looney Tunes"
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var coyote_time_ticks := 3.0
+var coyote_timer := -1.0
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var jump_buffer_ticks := 3.0
+var jump_buffer_timer := -1.0
+@export_range(0, 60, 1, "or_greater", "suffix:ticks") var action_buffer_ticks := 3.0
+var action_buffer_timer := -1.0
+@export var fallthrough_ignore_ticks_curve: Curve
+var fallthrough_ignore_timer := 0.0
+
+## If the player hits their head on a corner then how far can they be nudged out of the way
+@export_range(0, 16, 1, "or_greater", "suffix:px") var max_bonknudge_distance := 8.0
+@export var allow_backward_bonknudge := false
+
+@export_group("Climbing", "climb_")
+@export var climb_speed_vertical := 4.0
+@export var climb_speed_horizontal := 4.0
+@export var climb_coyote_time := 3.0
+var is_climbing := false
+
+@export_group("Wallslide")
+@export var wallslide_fall_speed := 1.0
+@export var walljump_horizontal_speed := 4.0
+@export var walljump_coyote_time := 3.0
+var wallslide_norm := 0
+var last_walljump_norm := 0
+
+@export_group("Water", "water_")
+@export_range(0, 16, 1, "or_greater", "suffix:px") var water_surface_jump_height := 24.0
+var water_surface_jump_vel := 0.0
+@export var water_ratio := 0.5
+@export var water_gravity_ratio := 0.5
+@export var water_max_fall_ratio := 0.5
+
+@export_group("Extra", "extra_")
+@export var extra_bounce := 0.0
+@export var extra_ground_dec := 0.0
+@export var extra_ground_smooth := 0.0
+@export var extra_air_dec := 0.0
+@export var extra_air_smooth := 0.0
+
+
+@onready var climb_area: Area2D = %"Climb Area"
+@onready var flip_node: Node2D = %"Flip"
+@onready var sprite: Sprite2D = %"Sprite"
+@onready var water_area: Area2D = %"Water Area"
+
+## Temporary
+@onready var og_spawn_position := global_position
+
+
+@onready var gamepad := Gamepad.create(-2)
+
+var age := 0.0
+
+var is_dead := false
+var is_grounded := false
+var in_water := false
+var direction := 1.0
+var jumped_from_ladder := false
+var was_on_wall := false
+
+var last_vel := Vector2.ZERO
+var vel_move := 0.0
+var vel_extra := 0.0
+
+
+func _enter_tree() -> void:
+	calculate_physics()
+
+
+func calculate_physics() -> void:
+	jump_gravity = Math.jump_gravity(jump_height_max, jump_ticks * 2 / Global.TPS)
+	fall_gravity = Math.jump_gravity(jump_height_max, fall_ticks * 2 / Global.TPS)
+	
+	jump_velocity_min = Math.jump_velocity(jump_height_min, jump_gravity)
+	jump_velocity_max = Math.jump_velocity(jump_height_max, jump_gravity)
+	water_surface_jump_vel = Math.jump_velocity(water_surface_jump_height, jump_gravity)
+	
+	max_fall_speed = jump_velocity_max * max_fall_ratio
+	
+	Global.gravity = fall_gravity
+
+
+func _physics_process(delta: float) -> void:
+	if Global.debug_physics:
+		queue_redraw()
+	
+	age += delta
+	
+	gamepad.poll(delta)
+	
+	if not is_dead:
+		_process_physics(delta)
+
+
+func _process_physics(delta: float) -> void:
+	if test_move(
+		transform.translated(Vector2.UP * 2.0).scaled_local(Vector2.ONE * 0.1),
+		Vector2.UP, null, 0.0, true):
+		die()
+	
+	jump_buffer_timer -= delta
+	if gamepad.jump.pressed:
+		jump_buffer_timer = jump_buffer_ticks / Global.TPS
+	
+	action_buffer_timer -= delta
+	if gamepad.action.pressed:
+		action_buffer_timer = action_buffer_ticks / Global.TPS
+	
+	if wallslide_norm != 0 and coyote_timer > 0.0:
+		direction = wallslide_norm
+	elif (is_on_floor() or allow_midair_flip) and not is_zero_approx(gamepad.move.x):
+		direction = sign(gamepad.move.x)
+	
+	if flip_node:
+		flip_node.scale.x = direction
+	
+	var was_in_water := in_water
+	in_water = water_area.get_overlapping_areas().size() > 0
+	
+	if was_in_water and not in_water:
+		# Leaving water
+		if is_jumping:
+			velocity.y = minf(velocity.y, -water_surface_jump_vel)
+	
+	if is_climbing:
+		process_state_climb(delta)
+	else:
+		process_state_platformer(delta)
+	
+	fallthrough_ignore_timer -= delta
+
+
+func process_state_platformer(delta: float) -> void:
+	if is_on_floor():
+		is_jumping = false
+		wallslide_norm = 0
+		last_walljump_norm = 0
+	
+	if (
+		climb_area.get_overlapping_areas().size() + climb_area.get_overlapping_bodies().size() > 0
+		and gamepad.move.y < 0.0
+		and velocity.y > -climb_speed_vertical
+	):
+		is_climbing = true
+		is_jumping = false
+		wallslide_norm = 0
+		last_walljump_norm = 0
+		return
+	
+	process_movement(delta)
+	process_gravity(delta)
+	process_wallslide(delta)
+	process_jump(delta)
+	
+	move(delta)
+
+
+func process_state_climb(delta: float) -> void:
+	if climb_area.get_overlapping_bodies().size() == 0 or (is_on_floor() and gamepad.move.y > 0.0):
+		is_climbing = false
+		coyote_timer = climb_coyote_time / Global.TPS
+		if gamepad.move.y < 0.0:
+			apply_floor_snap()
+			velocity.y = 0.0
+		return
+	
+	vel_extra = 0.0
+	velocity.y = gamepad.move.y * climb_speed_vertical
+	vel_move = gamepad.move.x * climb_speed_horizontal
+	
+	if jump_buffer_timer > 0.0:
+		jump_buffer_timer = -1.0
+		coyote_timer = -1.0
+		velocity.y = -jump_velocity_max
+		is_jumping = true
+		is_climbing = false
+		return
+	
+	move(delta)
+
+
+func move(delta: float) -> void:
+	var engine_vel = velocity.x - last_vel.x
+	vel_extra += engine_vel
+	velocity.x = vel_move + vel_extra + engine_vel
+	
+	last_vel.y = velocity.y
+	
+	if in_water:
+		velocity *= water_ratio
+	move_and_slide()
+	if in_water:
+		velocity /= water_ratio
+	
+	last_vel.x = velocity.x
+
+
+func process_movement(delta: float) -> void:
+	is_grounded = is_on_floor()
+	var is_input_opposing = not is_zero_approx(vel_move) and sign(vel_move) != sign(gamepad.move.x)
+	
+	var move_ticks := 0.0
+	var extra_dec := 0.0
+	var extra_smooth := 0.0
+	if is_grounded: # grounded movement
+		if gamepad.move.x == 0.0:
+				move_ticks = -move_dec_ticks
+		else:
+			if is_input_opposing: # opposing movement
+				move_ticks = move_opp_ticks
+			else:
+				move_ticks = move_acc_ticks
+		extra_dec = extra_ground_dec
+		extra_smooth = extra_ground_smooth
+	else: # air movement
+		if gamepad.move.x == 0.0:
+			move_ticks = -move_dec_air_ticks
+		else:
+			if is_input_opposing: # opposing movement
+				move_ticks = move_opp_air_ticks
+			else:
+				move_ticks = move_acc_air_ticks
+		extra_dec = extra_air_dec
+		extra_smooth = extra_air_smooth
+	
+		if move_ticks > 0.0:
+			var speed: float = move_speed / (move_ticks / Global.TPS / delta)
+			vel_move += gamepad.move.x * speed
+		elif move_ticks < 0.0:
+			vel_move = move_toward(vel_move, 0.0, move_speed / (-move_ticks / Global.TPS / delta))
+		
+		var max_speed := move_speed
+		vel_move = clamp(vel_move, -max_speed, max_speed)
+	
+	vel_extra = move_toward(vel_extra, 0.0, extra_dec * delta)
+	vel_extra = lerp(vel_extra, 0.0, Math.smooth(extra_smooth, delta))
+	
+	var hit_wall_on_left := is_on_wall() and test_move(transform, Vector2.LEFT)
+	var hit_wall_on_right := is_on_wall() and test_move(transform, Vector2.RIGHT)
+	
+	if hit_wall_on_left:
+		if vel_move < 0.0:
+			vel_move = 0.0
+		if vel_extra < 0.0:
+			if gamepad.move.x < 0.0:
+				vel_extra = 0.0
+			else:
+				vel_extra = -vel_extra * extra_bounce
+	
+	if hit_wall_on_right:
+		if vel_move > 0.0:
+			vel_move = 0.0
+		if vel_extra > 0.0:
+			if gamepad.move.x > 0.0:
+				vel_extra = 0.0
+			else:
+				vel_extra = -vel_extra * extra_bounce
+
+
+func process_gravity(delta: float) -> void:
+	# Ceiling bonk
+	if velocity.y < 0.0 and test_move(transform.translated(Vector2.UP), Vector2.UP * -velocity * delta):
+		if not slide_on_ceiling and get_last_slide_collision() != null:
+			if velocity.y < 0.0:
+				velocity.y = -velocity.y * bonk_bounce
+		if not try_bonknudge(max_bonknudge_distance * direction):
+			if (
+					not allow_backward_bonknudge or
+					not try_bonknudge(-max_bonknudge_distance * direction)
+			):
+				if velocity.y < 0.0 and is_on_ceiling():
+					velocity.y = -velocity.y * bonk_bounce
+	
+	# controllable jump height
+	if is_jumping and not gamepad.jump.down:
+		if velocity.y < -jump_velocity_min:
+			velocity.y = -jump_velocity_min
+	
+	# Fallthrough semisolid platforms when jumping while holding down
+	if is_on_floor():
+		if fallthrough_ignore_timer > 0.0:
+			position += -get_floor_normal()
+			# fallthrough_ignore_timer = fallthrough_ignore_ticks / Global.TPS
+			velocity.y = maxf(velocity.y, jump_velocity_min)
+		else:
+			velocity.y = minf(velocity.y, 0.0)
+			coyote_timer = coyote_time_ticks / Global.TPS
+	else:
+		# Apply gravity in air
+		var gravity = fall_gravity if velocity.y >= 0.0 else jump_gravity
+		gravity *= Global.gravity_scale
+		velocity.y += gravity * delta * (water_gravity_ratio if in_water else 1.0)
+	
+	# Cap fall speed
+	velocity.y = minf(
+			velocity.y,
+			max_fall_speed * (water_max_fall_ratio if in_water else 1.0)
+	)
+
+
+func try_bonknudge(distance: float) -> bool:
+	var x := 0.0
+	while x != distance:
+		if not test_move(transform.translated(Vector2(x, 0.0)), Vector2.UP):
+			position.x += x
+			return true
+		x = move_toward(x, distance, 1.0)
+	return false
+
+
+func process_jump(delta: float) -> void:
+	coyote_timer -= delta
+	
+	if fallthrough_ignore_timer > 0.0:
+		if climb_area.get_overlapping_areas().size() + climb_area.get_overlapping_bodies().size() > 0:
+			fallthrough_ignore_timer = 0.0
+			is_climbing = true
+	
+	if jump_buffer_timer > 0.0:
+		if gamepad.crouch.down and is_on_floor(): # if crouching then fall though
+			#SoundBank.play("fallthrough", position)
+			# position += -get_floor_normal()
+			position.y += 2.0
+			jump_buffer_timer = 0.0
+			coyote_timer = 0.0
+			var offset := abs(get_floor_normal().x)
+			var fallthrough_ignore_ticks := fallthrough_ignore_ticks_curve.sample_baked(offset)
+			fallthrough_ignore_timer = fallthrough_ignore_ticks / Global.TPS
+			# velocity.y = maxf(velocity.y, jump_velocity_min)
+			# velocity.y = 0.0
+		elif coyote_timer > 0.0: # or else jump
+			#SoundBank.play("jump", position)
+			is_jumping = true
+			coyote_timer = 0.0
+			jump_buffer_timer = 0.0
+			
+			velocity.y = -jump_velocity_max
+			if wallslide_norm != 0:
+				last_walljump_norm = wallslide_norm
+				vel_extra = wallslide_norm * walljump_horizontal_speed
+				wallslide_norm = 0
+
+
+func process_wallslide(delta: float) -> void:
+	if (
+			in_water
+			or velocity.y <= 0.0
+			or is_on_floor()
+			or not test_move(
+				transform.translated(Vector2.UP),
+				Vector2(sign(gamepad.move.x),
+				1.0)
+			)
+			or not is_on_wall()
+	):
+		was_on_wall = false
+		return
+	
+	var input_norm := signi(gamepad.move.x)
+	var wall_norm := get_wall_normal()
+	if not (
+			abs(wall_norm.x) >= 0.95
+			and wall_norm.y <= 0.0
+	):
+		was_on_wall = false
+		return
+	
+	was_on_wall = true
+	wallslide_norm = signi(wall_norm.x)
+	
+	if (
+			not was_on_wall
+			or last_walljump_norm == wallslide_norm
+			or wallslide_norm != -input_norm
+		): return
+	
+	if velocity.y > wallslide_fall_speed:
+		velocity.y = wallslide_fall_speed
+	coyote_timer = walljump_coyote_time / Global.TPS
+
+
+func grounded_refresh() -> void:
+	was_on_wall = false
+	wallslide_norm = 0
+	last_walljump_norm = 0
+	jumped_from_ladder = false
+	fallthrough_ignore_timer = 0.0
+	is_jumping = false
+
+
+func airborne_refresh() -> void:
+	grounded_refresh()
+	coyote_timer = 0.0
+
+
+func _draw() -> void:
+	if not Global.debug_physics: return
+	
+	draw_line(Vector2.ZERO, Vector2(vel_move, 0.0), Color.WHITE, 2.0)
+	draw_line(Vector2(0.0, -8.0), Vector2(vel_extra, -8.0), Color.RED, 2.0)
+	draw_line(Vector2.ZERO, Vector2(0.0, velocity.y), Color.GREEN, 2.0)
+
+
+# -------------------------------------------------------------------------------- #
+
+
+func die() -> void:
+	if is_dead: return
+	is_dead = true
+	
+	SFX.event("death.player").at(self).play()
+	
+	sprite.frame = 0
+	
+	respawn()
+
+
+func respawn() -> void:
+	modulate.a = 0.5
+	z_index += 1000
+	scale = Vector2.ONE * 0.75
+	
+	var spawn_position := og_spawn_position
+	
+	var delta := spawn_position - position
+	
+	var duration := 1.0
+	
+	var tween := create_tween()
+	tween.tween_property(sprite, "rotation", TAU * 4.0, duration).from(0.0)\
+			.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(self, "position", spawn_position, duration)\
+			.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	
+	await tween.finished
+	
+	tween.kill()
+	
+	modulate.a = 1.0
+	z_index -= 1000
+	scale = Vector2.ONE
+	vel_move = 0.0
+	vel_extra = 0.0
+	velocity = Vector2.ZERO
+	last_vel = Vector2.ZERO
+	is_dead = false
---
+++ scripts/types/gamepad.gd
@@ -0,0 +1,304 @@
+class_name Gamepad extends RefCounted
+
+
+const DEVICE_AUTO = -2
+const DEVICE_KEYBOARD = -1
+
+
+class JoyInfo:
+	var id := 0
+	var name := ""
+	var guid := ""
+	var is_known := false
+	var connected := false
+	
+	static func from_id(id: int) -> JoyInfo:
+		var info := JoyInfo.new()
+		info.id = id
+		info.name = Input.get_joy_name(id)
+		info.guid = Input.get_joy_guid(id)
+		info.is_known = Input.is_joy_known(id)
+		return info
+
+
+static var joys: Dictionary = {}
+static var gamepads: Array[Gamepad] = []
+
+static var keyboard := Gamepad.create(DEVICE_KEYBOARD)
+
+
+static func _static_init() -> void:
+	gamepads.append(keyboard)
+
+
+static func _joy_connection_changed(id: int, connected: bool) -> void:
+	if connected:
+		var info := JoyInfo.from_id(id)
+		info.connected = true
+		joys[id] = info
+		print("[Input] ",info.name," connected as ",id)
+		# if info.is_known:
+		# 	Toast.show(str("Connected: ",info.name))
+		# else:
+		# 	Toast.show(str("Unknown Gamepad: ",info.name))
+		var gamepad := Gamepad.create(id)
+		gamepads.append(gamepad)
+		# Bus.gamepad_connected.emit(gamepad)
+		return
+	
+	if not joys.has(id): return
+	var info: JoyInfo = joys[id]
+	info.connected = false
+	print("[Input] ",info.name," disconnected from ",id)
+	# Toast.show(str("Disconnected: ",info.name))
+	var index := Util.index_of(gamepads, func(gamepad: Gamepad) -> bool: return gamepad.device == id)
+	var gamepad := gamepads[index]
+	# Bus.gamepad_disconnected.emit(gamepad)
+	gamepads.remove_at(index)
+
+
+static func create(device: int) -> Gamepad:
+	var gamepad := Gamepad.new()
+	gamepad.device = device
+	return gamepad
+
+
+# ---------------------------------------- #
+
+
+@export var device := 0
+@export var deadzone := 0.5
+@export var move_deadzone := 0.35
+@export var crouch_threshold := 0.7
+@export var move_snap_amount := 1.3333
+@export var aim_deadzone := 0.5
+@export var trigger_deadzone := 0.5
+
+
+var any := Btn.new()
+
+var menu_ok := Btn.new()
+var menu_back := Btn.new()
+var menu_pause := Btn.new()
+var menu_left := Btn.turbo()
+var menu_right := Btn.turbo()
+var menu_up := Btn.turbo()
+var menu_down := Btn.turbo()
+
+var move := Vector2.ZERO
+var aim := Vector2.ZERO
+
+var jump := Btn.new()
+var crouch := Btn.new()
+
+var action := Btn.new()
+var pickup := Btn.new()
+var switch := Btn.new()
+
+var self_destruct := Btn.new()
+
+
+func duplicate() -> Gamepad:
+	return Gamepad.create(self.device)
+
+
+func get_connection() -> bool:
+	match device:
+		-2: return true
+		-1: return true
+	if Gamepad.joys.has(device):
+		return Gamepad.joys[device].connected
+	return false
+
+
+func get_name() -> String:
+	match device:
+		-2: return "Auto"
+		-1: return "Keyboard"
+	if not Gamepad.joys.has(device):
+		return Gamepad.joys[device].name
+	return "Unknown"
+
+
+func vibrate(weak: float, strong: float, duration: float) -> void:
+	Input.start_joy_vibration(device, weak, strong, duration)
+
+
+func poll(delta: float) -> void:
+	match device:
+		-2:
+			if joys.has(0):
+				var _device := device
+				device = 0
+				poll_gamepad(delta)
+				device = _device
+			else:
+				poll_keyboard(delta)
+		-1:
+			poll_keyboard(delta)
+		_:
+			poll_gamepad(delta)
+
+
+func poll_gamepad(delta: float) -> void:
+	any.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_START)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_BACK)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_A)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_B)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_X)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_Y)
+	), delta)
+	
+	menu_ok.track(Input.is_joy_button_pressed(device, JOY_BUTTON_B), delta)
+	menu_back.track(Input.is_joy_button_pressed(device, JOY_BUTTON_A), delta)
+	menu_pause.track(Input.is_joy_button_pressed(device, JOY_BUTTON_START), delta)
+	
+	menu_left.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_LEFT) or
+			Input.get_joy_axis(device, JOY_AXIS_LEFT_X) <= -deadzone
+	), delta)
+	menu_right.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_RIGHT) or
+			Input.get_joy_axis(device, JOY_AXIS_LEFT_X) >= +deadzone
+	), delta)
+	menu_up.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_UP) or
+			Input.get_joy_axis(device, JOY_AXIS_LEFT_Y) <= -deadzone
+	), delta)
+	menu_down.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_DOWN) or
+			Input.get_joy_axis(device, JOY_AXIS_LEFT_Y) >= +deadzone
+	), delta)
+	
+	move.x = Input.get_joy_axis(device, JOY_AXIS_LEFT_X)
+	move.y = Input.get_joy_axis(device, JOY_AXIS_LEFT_Y)
+	aim.x = Input.get_joy_axis(device, JOY_AXIS_RIGHT_X)
+	aim.y = Input.get_joy_axis(device, JOY_AXIS_RIGHT_Y)
+	
+	move.x += float(Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_RIGHT)) - float(Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_LEFT))
+	move.y += float(Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_DOWN)) - float(Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_UP))
+	
+	move.x = clampf(move.x, -1.0, 1.0)
+	move.y = clampf(move.y, -1.0, 1.0)
+	
+	# apply deadzone to account for shitty controller joysticks
+	var move_length_squared := move.length_squared()
+	if move_length_squared < move_deadzone ** 2.0:
+		move = Vector2.ZERO
+	else:
+		# remap move vector to hide the deadzone
+		# 25...100 -> 0...100
+		var move_length := sqrt(move_length_squared)
+		move = (move / move_length) * ((move_length - move_deadzone) / (1.0 - move_deadzone))
+	
+	move = round(move.normalized() * move_snap_amount)
+	
+	jump.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_A)
+	), delta)
+	
+	crouch.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_DPAD_DOWN)
+			or Input.get_joy_axis(device, JOY_AXIS_LEFT_Y) > crouch_threshold
+	), delta)
+	
+	action.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_X)
+			or Input.get_joy_axis(device, JOY_AXIS_TRIGGER_LEFT) >= trigger_deadzone
+			or Input.get_joy_axis(device, JOY_AXIS_TRIGGER_RIGHT) >= trigger_deadzone
+	), delta)
+	pickup.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_B)
+	), delta)
+	switch.track((
+			Input.is_joy_button_pressed(device, JOY_BUTTON_Y)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_LEFT_SHOULDER)
+			or Input.is_joy_button_pressed(device, JOY_BUTTON_RIGHT_SHOULDER)
+	), delta)
+	
+	self_destruct.track(Input.is_joy_button_pressed(device, JOY_BUTTON_BACK), delta)
+
+
+func poll_keyboard(delta: float) -> void:
+	any.track((
+			Input.is_key_pressed(KEY_SPACE)
+			or Input.is_key_pressed(KEY_ENTER)
+			or Input.is_key_pressed(KEY_E)
+	), delta)
+	
+	menu_ok.track((
+			Input.is_physical_key_pressed(KEY_E)
+			or Input.is_physical_key_pressed(KEY_SPACE)
+			or Input.is_physical_key_pressed(KEY_ENTER)
+	), delta)
+	menu_back.track((
+			Input.is_physical_key_pressed(KEY_Q)
+			or Input.is_physical_key_pressed(KEY_BACKSPACE)
+			or Input.is_physical_key_pressed(KEY_ESCAPE)
+	), delta)
+	menu_pause.track(Input.is_key_label_pressed(KEY_ESCAPE), delta)
+	
+	menu_left.track((
+			Input.is_physical_key_pressed(KEY_A)
+			or Input.is_physical_key_pressed(KEY_LEFT)
+	), delta)
+	menu_right.track((
+			Input.is_physical_key_pressed(KEY_D)
+			or Input.is_physical_key_pressed(KEY_RIGHT)
+	), delta)
+	menu_up.track((
+			Input.is_physical_key_pressed(KEY_W)
+			or Input.is_physical_key_pressed(KEY_UP)
+	), delta)
+	menu_down.track((
+			Input.is_physical_key_pressed(KEY_S)
+			or Input.is_physical_key_pressed(KEY_DOWN)
+	), delta)
+	
+	var right := float(
+			Input.is_physical_key_pressed(KEY_D)
+			or Input.is_physical_key_pressed(KEY_RIGHT)
+	)
+	var left := float(
+			Input.is_physical_key_pressed(KEY_A)
+			or Input.is_physical_key_pressed(KEY_LEFT)
+	)
+	move.x = right - left
+	
+	var down := float(
+			Input.is_physical_key_pressed(KEY_S)
+			or Input.is_physical_key_pressed(KEY_DOWN)
+	)
+	var up := float(
+			Input.is_physical_key_pressed(KEY_W)
+			or Input.is_physical_key_pressed(KEY_UP)
+	)
+	move.y = down - up
+	
+	jump.track((
+			Input.is_physical_key_pressed(KEY_Z)
+			or Input.is_physical_key_pressed(KEY_SPACE)
+	), delta)
+	crouch.track((
+			Input.is_physical_key_pressed(KEY_S)
+			or Input.is_physical_key_pressed(KEY_DOWN)
+	), delta)
+	
+	action.track(
+			Input.is_physical_key_pressed(KEY_X)
+			or Input.is_physical_key_pressed(KEY_E)
+	, delta)
+	pickup.track(
+			Input.is_physical_key_pressed(KEY_C)
+			or Input.is_physical_key_pressed(KEY_CTRL)
+			or Input.is_physical_key_pressed(KEY_F)
+	, delta)
+	switch.track(
+			Input.is_physical_key_pressed(KEY_V)
+			or Input.is_physical_key_pressed(KEY_ALT)
+			or Input.is_physical_key_pressed(KEY_Q)
+			or Input.is_physical_key_pressed(KEY_R)
+	, delta)
+	
+	self_destruct.track(Input.is_key_label_pressed(KEY_DELETE), delta)
