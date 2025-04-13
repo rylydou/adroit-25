3d90a9e022b32984ad900e3600a023202aeaf224
Blake Bukowsky <71535346+BlakeBukowsky@users.noreply.github.com>
2025-04-12 21:42:17 -0500

    World fixes and enemy fixes

--- scripts/nodes/enemy.gd
+++ scripts/nodes/enemy.gd
@@ -3,17 +3,18 @@ extends PathFollow2D
 @export var speed:float
 @export var sprite: Sprite2D
 @export var anim_tree: AnimationTree
+@export var anim_speed:= 1.0
 @export var variability: float
 var oldpos
 
 func _ready():
-	anim_tree.tree_root.start_offset = randf()
-	anim_tree.tree_root.timeline_length = (1 - variability/2) + randf() * variability
+	anim_tree.tree_root.start_offset = randf() * anim_speed
+	anim_tree.tree_root.timeline_length = (anim_speed - variability/2) + randf() * variability
 
 func _process(delta: float) -> void:
 	oldpos = global_position
 	progress += speed * delta
-	if global_position.x - oldpos.x < -0.25:
+	if is_zero_approx(global_position.x - oldpos.x):
 		if sprite.flip_h != null:
 			sprite.flip_h = true
 	else:
